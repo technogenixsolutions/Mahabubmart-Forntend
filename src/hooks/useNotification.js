d7745f6e0259105801a018d7b35cccabbbe55648
freelancermahabubs <info.mahabubalom23@gmail.com>
2026-03-13 23:49:54 +0600

    addd

--- src/hooks/useNotification.js
+++ src/hooks/useNotification.js
@@ -11,7 +11,7 @@ const useNotification = () => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io(import.meta.env.VITE_APP_API_BASE_URL));
+    setSocket(io(import.meta.env.NEXT_PUBLIC_API_BASE_URL));
     // setSocket(io("http://localhost:5055"));
   }, []);
 
