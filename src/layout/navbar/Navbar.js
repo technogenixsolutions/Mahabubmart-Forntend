eef97b7574c04a361a2147e98d4bdb2732e540df
freelancermahabubs <info.mahabubalom23@gmail.com>
2026-03-14 16:05:52 +0600

    jk

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -67,7 +67,7 @@ const Navbar = () => {
               <a className="mr-3 lg:mr-12 xl:mr-12 hidden md:hidden lg:block">
                 <Image
                   width={120}
-                  height={45}
+                  height={100}
                   src="https://res.cloudinary.com/dgwwhniph/image/upload/v1773482630/cno4dflj4kz83rim1qbi.png"
                   alt="mahabubmart"
                 />
