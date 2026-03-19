eb8524751cd347c89c03880ba7f31b5e446bc320
freelancermahabubs <info.mahabubalom23@gmail.com>
2026-03-19 10:25:02 +0600

    add

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -252,7 +252,7 @@ const ContactUs = () => {
                   <div className="relative">
                     <button
                       data-variant="flat"
-                      className="md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold text-center justify-center border-0 border-transparent rounded-md placeholder-white focus-visible:outline-none focus:outline-none bg-emerald-500 text-white px-5 md:px-6 lg:px-8 py-3 md:py-3.5 lg:py-3 hover:text-white hover:bg-emerald-600 h-12 mt-1 text-sm lg:text-base w-full sm:w-auto"
+                      className="md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold text-center justify-center border-0 border-transparent rounded-md placeholder-white focus-visible:outline-none focus:outline-none  text-white px-5 md:px-6 lg:px-8 py-3 md:py-3.5 lg:py-3 hover:text-white bg-gradient-to-r from-[#1F6BBF] via-[#279FDF] to-[#00a4db] hover:from-[#155a9e] hover:via-[#1e88c8] hover:to-[#0090c2]  h-12 mt-1 text-sm lg:text-base w-full sm:w-auto"
                     >
                       {t("common:contact-page-form-send-btn")}
                     </button>
