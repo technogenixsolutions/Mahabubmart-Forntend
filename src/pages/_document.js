d3d005235aeef785be2e020088ea7c5cccc0ca02
freelancermahabubs <info.mahabubalom23@gmail.com>
2026-03-27 09:36:45 +0600

    addd

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -27,7 +27,7 @@ class MyDocument extends Document {
             property="og:description"
             content="Shop electronics, gadgets, home appliances and daily essentials from Mahabubmart with great deals and fast delivery across Bangladesh."
           />
-          <meta name="facebook-domain-verification" content="5mxlj583bsjbxc2blfg7zu89sosolx" />
+          {/* <meta name="facebook-domain-verification" content="5mxlj583bsjbxc2blfg7zu89sosolx" /> */}
           <meta property="og:url" content="https://www.mahabubmart.com/" />
           <meta
             property="og:image"
